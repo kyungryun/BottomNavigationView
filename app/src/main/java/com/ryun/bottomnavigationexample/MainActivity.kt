ad68b93b8d2edc94e68651086734a962c2e9e551
kyungryun <kyung_ryun@naver.com>
2021-02-15 15:07:43 +0900

    navigationadvancedsample 기본 형태 셋팅

--- app/src/main/java/com/ryun/bottomnavigationexample/MainActivity.kt
+++ app/src/main/java/com/ryun/bottomnavigationexample/MainActivity.kt
@@ -3,41 +3,67 @@ package com.ryun.bottomnavigationexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.Collections.list
 
+
+/**
+ * An activity that inflates a layout that has a [BottomNavigationView].
+ */
 class MainActivity : AppCompatActivity() {
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var navController: NavController
+
+    private var currentNavController: LiveData<NavController>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        } // Else, need to wait for onRestoreInstanceState
+    }
 
-        appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.homeFragment,R.id.searchFragment,R.id.menuFragment)
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    /**
+     * Called on first creation and when restoring state.
+     */
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
+
+        val navGraphIds = listOf(R.navigation.nav_graph_home, R.navigation.nav_graph_search, R.navigation.nav_graph_menu)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
         )
 
-        navController = nav_host_fragment.findNavController()
-
-        navController.addOnDestinationChangedListener{ _, destination, _ ->
-            when(destination.id){
-                R.id.startFragment -> setBottomNavVisibility(false)
-                else -> setBottomNavVisibility(true)
-            }
-        }
-        bottom_nav_bar.setupWithNavController(navController)
+        // Whenever the selected controller changes, setup the action bar.
+        controller.observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+        })
+        currentNavController = controller
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
-    }
-
-    private fun setBottomNavVisibility(isVisible: Boolean){
-        bottom_nav_bar.isVisible = isVisible
+        return currentNavController?.value?.navigateUp() ?: false
     }
 }
